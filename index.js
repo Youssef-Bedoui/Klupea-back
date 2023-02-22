4bbf2c29c57c27fb85342131e263df836dc84795
Youssef-Bedoui <Jozefbedoui@gmail.com>
2023-02-22 23:55:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://klupea-e-commerce-app.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(cors({
     origin: "https://klupea-e-commerce-app.onrender.com",
     methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],

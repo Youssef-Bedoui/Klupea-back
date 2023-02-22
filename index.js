e3796b6b012d30f341174c3c79c453242be384c1
Youssef-Bedoui <Jozefbedoui@gmail.com>
2023-02-22 22:45:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://klupea-e-commerce-app.onrender.com",
     methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
     credentials: true,
 }));
